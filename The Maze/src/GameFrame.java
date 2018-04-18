7ad3b83d82ff2b968b4a24ce4590b6a101b2d78f
Adrian Cieśla <Adrian Ciela@192.168.0.10>
2018-04-18 20:44:21 +0200

    Nałożyłem na ramkę grafikę z klasy GameGraphics

--- The Maze/src/GameFrame.java
+++ The Maze/src/GameFrame.java
@@ -4,9 +4,11 @@ public class GameFrame {
 
 	public void run() {
 		JFrame frame = new JFrame();
+		GameGraphics gg = new GameGraphics();
 		frame.setBounds(100, 50, 1100, 600);
 		frame.setResizable(false);
 		frame.setTitle("Labirynt");
 		frame.setVisible(true);
+		frame.add(gg);
 	}
 }
