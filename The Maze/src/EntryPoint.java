7c90dd4a01623353213c813d86f837045a28ab17
Adrian Cieśla <Adrian Ciela@192.168.0.10>
2018-04-18 20:08:11 +0200

    Dodałem ramkę do menu gry

--- The Maze/src/EntryPoint.java
+++ The Maze/src/EntryPoint.java
@@ -4,7 +4,7 @@ public class EntryPoint {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setBounds(100, 50, 1100, 600);
+		frame.setBounds(100, 50, 500, 600);
 		frame.setResizable(false);
 		frame.setTitle("Labirynt");
 		frame.setVisible(true);
