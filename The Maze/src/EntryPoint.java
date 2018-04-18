b5beec9ee68b0b96f12a6e258b5ef9072be36879
Adrian Cieśla <Adrian Ciela@192.168.0.10>
2018-04-18 20:43:14 +0200

    Małe zmiany

--- The Maze/src/EntryPoint.java
+++ The Maze/src/EntryPoint.java
@@ -4,11 +4,12 @@ public class EntryPoint {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		GameFrame gf = new GameFrame();
 		frame.setBounds(100, 50, 500, 600);
 		frame.setResizable(false);
 		frame.setTitle("Labirynt");
-		frame.setVisible(true);
-
+		//Mo¿na dodaæ jak¹œ grafikê do menu potem ale narazie bêdzie w³¹cza³a siê przez t¹ klasê ramka planszy
+		frame.setVisible(false); // ustawi³em narazie na false bo nie ma jeszcze grafiki
+		gf.run();
 	}
-
 }
