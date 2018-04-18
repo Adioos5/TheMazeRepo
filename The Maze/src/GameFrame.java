3261ccd718bd3238aa315f42412a76bc94a6cd5e
Adrian Cieśla <Adrian Ciela@192.168.0.10>
2018-04-18 20:11:25 +0200

    Dodałem ramkę do pierwszej planszy

---
+++ The Maze/src/GameFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class GameFrame {
+
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.setBounds(100, 50, 1100, 600);
+		frame.setResizable(false);
+		frame.setTitle("Labirynt");
+		frame.setVisible(true);
+	}
+}
