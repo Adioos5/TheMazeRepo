be80ed4ca2894ab7281aa4348d2e4cb6e9bc046a
Bartłomiej Rasztabiga <EEE@eee.ep>
2018-04-19 08:14:36 +0200

    zmieniam cos

---
+++ src/EntryPoint.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		GameFrame gf = new GameFrame();
+		frame.setBounds(100, 50, 500, 600);
+		frame.setResizable(false);
+		frame.setTitle("Labirynt");
+		//Mo¿na dodaæ jak¹œ grafikê do menu potem ale narazie bêdzie w³¹cza³a siê przez t¹ klasê ramka planszy
+		frame.setVisible(false); // ustawi³em narazie na false bo nie ma jeszcze grafiki
+		gf.run();
+	}
+}
