b46154e9ca38cd60973ee2119cfe484d69fc48df
Hamthreer <seweryn@vp.pl>
2018-06-13 23:03:57 +0200

    Changed Help Window a bit

--- src/main/java/pl/com/lo/maze/windows/HelpWindow.java
+++ src/main/java/pl/com/lo/maze/windows/HelpWindow.java
@@ -12,7 +12,7 @@ public class HelpWindow extends JFrame implements ActionListener {
     private JButton bBack;
 
     public HelpWindow() {
-        setBounds(200, 50, 500, 500);
+        setBounds(200, 50, 460, 500);
         setTitle("Help");
         setLayout(null);
         bBack = new JButton("Back");
@@ -20,30 +20,34 @@ public class HelpWindow extends JFrame implements ActionListener {
         bBack.addActionListener(this);
         add(bBack);
 
-        JLabel lb1 = new JLabel("Controlls");
-        lb1.setBounds(200, 20, 100, 50);
+        JLabel lb1 = new JLabel("Controls");
+        lb1.setBounds(205, 20, 100, 50);
         add(lb1);
 
         JLabel lb4 = new JLabel("Up - Up Arrow Key");
-        lb4.setBounds(100, 100, 500, 50);
+        lb4.setBounds(180, 100, 500, 50);
         add(lb4);
 
         JLabel lb5 = new JLabel("Down - Down Arrow Key");
-        lb5.setBounds(100, 125, 500, 50);
+        lb5.setBounds(160, 125, 500, 50);
         add(lb5);
 
         JLabel lb2 = new JLabel("Left - Left Arrow Key");
-        lb2.setBounds(100, 150, 500, 50);
+        lb2.setBounds(170, 150, 500, 50);
         add(lb2);
 
         JLabel lb3 = new JLabel("Right - Right Arrow Key");
-        lb3.setBounds(100, 175, 500, 50);
+        lb3.setBounds(165, 175, 500, 50);
         add(lb3);
-
+        
+        JLabel lbeg = new JLabel("There is an Easter Egg in game, look carefully!");
+        lbeg.setBounds(90, 430, 500, 50);
+        add(lbeg);
     }
 
     public void runHelpWindow() {
         setVisible(true);
+        setResizable(false);
     }
 
     @Override
